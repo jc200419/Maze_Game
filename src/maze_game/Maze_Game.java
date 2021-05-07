ce2bbd12bd8efb6dc748ba6e9d9fcd5595348a04
jc200419 <jc200419@P007D022.reigate.ac.uk>
2021-05-07 10:44:34 +0100

    very start

---
+++ src/maze_game/Maze_Game.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maze_game;
+
+/**
+ *
+ * @author jc200419
+ */
+public class Maze_Game {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
