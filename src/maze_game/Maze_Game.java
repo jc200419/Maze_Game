a34ab7f8bcd585efc7a74e1ca7185bdde61b82fa
jc200419 <jc200419@P007D022.reigate.ac.uk>
2021-05-07 11:09:16 +0100

    start of program

--- src/maze_game/Maze_Game.java
+++ src/maze_game/Maze_Game.java
@@ -1,21 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package maze_game;
+//CheckList -
+//1. Create the Maze through a random algorithem
 
-/**
- *
- * @author jc200419
- */
+import javax.imageio.ImageIO;
+
+//2. Make a character that can move within the map
+//3. figure out a start and end to the map
+//4. create monsters and chests 
+//5. crate a way for the monsters to move around randomly 
+//6. add a health bar and time limit 
+//7. add a pause menu and main menu 
+//8. make 3 difficulties (blocked vision) 
+//9. create 5 levels all with different atmospheres 
+//10. add sound and touch up any code. 
 public class Maze_Game {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        openMainMenu();
+    }
+    public static void openMainMenu(){
+        MainMenu menu = new MainMenu();
+        try{
+            menu.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("put code in here")))));
+        }catch (Exception e){
+            System.out.println("error");
+        }menu.setVisible(true);
+        
     }
     
+        
 }
